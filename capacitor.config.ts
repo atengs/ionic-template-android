0201b80c41833c3489a67947b6ad798ee6ce0b91
Rizky Adi Pratama <71449366+rzkadp@users.noreply.github.com>
2024-04-18 11:51:18 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.mtg_e_absensi',
+  appName: 'MTG e-Absensi',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http'
+  }
+};
+
+export default config;
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+
+export default {
+  components: {
+    IonApp, IonRouterOutlet
+  },
+
+  data() {
+    return {
+      api_path: 'http://localhost:8000/'
+    }
+  }
+}
+</script>
